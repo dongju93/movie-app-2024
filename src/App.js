65b92ec81f79b9fba4868c51397298657b7f1d36
dongju93 <spdlqj011@gmail.com>
2024-07-13 21:57:30 +0900

    #5.1 Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+// 컴포넌트 파일 내 function 을 import 하여 사용
+import Button from "./Button";
+import styles from "./App.module.css"
+
 function App() {
     return (
         <div>
-            <h1>Welcome back!!!!</h1>
+            <h1 className={styles.title}>Welcome back!!!!</h1>
+            <Button text={"this is text"} />
         </div>
     );
 }
---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import { PropTypes } from "prop-types";
+// React는 .module.css 파일을 JS Object 로 변환시킴
+import styles from "./Button.module.css";
+
+function Button({ text }) {
+    // class에 styles.btn 을 추가하여 CSS를 적용함
+    return <button className={styles.btn}>{text}</button>;
+}
+
+// Practice 와 동일하게 propTypes 를 사용하여 Type 및 필수 여부를 설정
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+
+export default Button;

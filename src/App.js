e1853f267636b9c7511f2bdbb781eb41928a49e2
dongju93 <spdlqj011@gmail.com>
2024-07-14 14:03:35 +0900

    #6.1 useEffect & #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 // 컴포넌트 파일 내 function 을 import 하여 사용
 import Button from "./Button";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+    const updateCount = () => {
+        setCount((prev) => prev + 1);
+    };
+    const [keyword, setKeyword] = useState("");
+    console.log("I run all the time");
+    const iRunOnlyOnce = () => {
+        console.log("Call API");
+    };
+    // input 에서 값을 입력받아 keyword 에 저장
+    const searchKeyword = (e) => {
+        setKeyword(e.target.value);
+    };
+    // useEffect 를 사용하여 할당된 함수(console.log)를 한 번만 실행, 두 번째 인자가 빈 배열 일때
+    // 두 번째 인자로 값을 넣어주면 해당 값이 변경될 때마다 해당 함수 실행
+    useEffect(() => {
+        // keyword 가 비어있지 않고 길이가 6보다 작을 때 검색 쿼리
+        if (keyword !== "" && keyword.length < 6) {
+            console.log("Search for short keyword:", keyword);
+            // keyword 가 비어있지 않고 길이가 6보다 클 때 검색 쿼리
+        } else {
+            console.log("Search for long keyword:", keyword);
+        }
+    }, [keyword]);
+    useEffect(iRunOnlyOnce, []);
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!!!!</h1>
-            <Button text={"this is text"} />
+            <input
+                onChange={searchKeyword}
+                type="text"
+                placeholder="Search for something"
+            />
+            <h1 className={styles.title}>{count}</h1>
+            <Button text={"Update count"} cntFunc={updateCount} />
         </div>
     );
 }
--- src/Button.js
+++ src/Button.js
@@ -2,14 +2,19 @@ import { PropTypes } from "prop-types";
 // React는 .module.css 파일을 JS Object 로 변환시킴
 import styles from "./Button.module.css";
 
-function Button({ text }) {
+function Button({ text, cntFunc }) {
     // class에 styles.btn 을 추가하여 CSS를 적용함
-    return <button className={styles.btn}>{text}</button>;
+    return (
+        <button className={styles.btn} onClick={cntFunc}>
+            {text}
+        </button>
+    );
 }
 
 // Practice 와 동일하게 propTypes 를 사용하여 Type 및 필수 여부를 설정
 Button.propTypes = {
     text: PropTypes.string.isRequired,
+    cntFunc: PropTypes.func.isRequired,
 };
 
 export default Button;
